38f50b596df5f81b3adf193f771b16058aed01c5
Douglas Marques <douglas.bmarques@hotmail.com>
2025-05-21 09:42:13 -0300

    Add Worker

--- server.ts
+++ server.ts
@@ -1,12 +1,38 @@
 import Fastify from 'fastify';
+import { Queue, Worker } from 'bullmq';
 
-const fastify = Fastify();
+const QUEUE_NAME = 'my-queue';
+const redisConnection = {
+  host: 'localhost',
+  port: 6379,
+}
 
-fastify.get('/', async (request, reply) => {
-  console.log('Request received');
-  return { message: 'hello world' };
-});
+const queue = new Queue(QUEUE_NAME, { connection: redisConnection });
 
-fastify.listen({ port: 3000 }, (_, address) => {
-  console.log(`Server is listening on ${address}`);
-});
+function buildServer() {
+  const fastify = Fastify();
+
+  fastify.get('/', async () => {
+    console.log('Request received');
+    
+    console.log('Enqueuing a job');
+    queue.add('my-job', {});
+
+    return { message: 'hello world' };
+  });
+
+  fastify.listen({ port: 3000 }, (_, address) => {
+    console.log(`Server is listening on ${address}`);
+  });
+}
+
+function runWorkers() {
+  new Worker('my-queue', async (job) => {
+    console.log('Processing job:', job.id);
+  }, {
+    connection:  redisConnection
+  });  
+}
+
+buildServer();
+runWorkers();
